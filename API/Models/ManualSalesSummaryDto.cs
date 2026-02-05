a84b9bb6e9e8ef26006019008772fd51f382c003
Davian Aguilar <davian-007@users.noreply.github.com>
2026-02-04 21:08:21 -0600

    API Folders
    API Folders added

---
+++ API/Models/ManualSalesSummaryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SalesSummaryApi.Models
+{
+    public class ManualSalesSummaryDto
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime SummaryDate { get; set; }
+        public int Total_Items { get; set; }
+        public decimal Total_Sales { get; set; }
+    }
+}
---
+++ API/Models/SalesSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SalesSummaryApi.Models
+{
+    public class SalesSummary
+    {
+        public int SalesSummaryId { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime SummaryDate { get; set; }
+        public int Total_Items { get; set; }
+        public decimal Total_Sales { get; set; }
+        public string Source { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
